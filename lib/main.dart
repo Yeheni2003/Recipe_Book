7233d8f9680a18bd5098de494734d640ce23678b
Yeheni2003 <yehenivishmini@icloud.com>
2025-03-24 22:31:58 +0530

    commits

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'Pages/RecipeListScreen.dart';
+
+void main() {
+  runApp(const RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe Book',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const RecipeListScreen(),
+    );
+  }
+}
